c467a33bc052c77dad54f72b82fd8eab0a12eb03
= <dmastergame74@gmail.com>
2025-02-24 19:28:57 -0300

    capture args from command line

--- src/main/java/dev/buskopan/Main.java
+++ src/main/java/dev/buskopan/Main.java
@@ -2,8 +2,12 @@ package dev.buskopan;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server(8080);
 
-        server.start("https://dummyjson.com/products");
+        int port = Integer.parseInt(args[0]);
+        String url = args[1];
+
+        Server server = new Server(port);
+
+        server.start(url);
     }
 }
